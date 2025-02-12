ddcb1eca83c8046deace58535e3c4f65af73d47c
Rébecca <rebecca@codingtheweb.ca>
2025-02-12 18:16:34 -0500

    feat: add index page

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import fastapi
 import uvicorn
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
 api = fastapi.FastAPI()
+templates = Jinja2Templates("templates")
+api.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @api.get("/")
-def index():
-    return "Hello Weather App"
+def index(request: Request):
+    return templates.TemplateResponse("home/index.html", {"request": request})
 
 
 if __name__ == "__main__":
