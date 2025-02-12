8c22c9282e0bf17f5a19079688cd8df541866df6
Rébecca <rebecca@codingtheweb.ca>
2025-02-12 18:52:36 -0500

    refactor: split app into different files

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import fastapi
 import uvicorn
-from starlette.requests import Request
+
 from starlette.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
+
+from api import weather_api
+from views import home
 
 api = fastapi.FastAPI()
-templates = Jinja2Templates("templates")
-api.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@api.get("/")
-def index(request: Request):
-    return templates.TemplateResponse("home/index.html", {"request": request})
+def configure():
+    configure_routing()
+
+
+def configure_routing():
+    api.mount("/static", StaticFiles(directory="static"), name="static")
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
 
 
 if __name__ == "__main__":
+    configure()
     uvicorn.run(api)
