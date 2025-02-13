3bcfe44bdcb8bbfd90056190fa1ec83c1483b07a
Rébecca <rebecca@codingtheweb.ca>
2025-02-12 20:04:09 -0500

    refactor: async api call

--- api/weather_api.py
+++ api/weather_api.py
@@ -10,8 +10,10 @@ router = fastapi.APIRouter()
 
 
 @router.get("/api/weather/{city}")
-def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
+async def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
 
-    report = openweather_service.get_report(loc.city, loc.state, loc.country, units)
+    report = await openweather_service.get_report(
+        loc.city, loc.state, loc.country, units
+    )
 
     return report
