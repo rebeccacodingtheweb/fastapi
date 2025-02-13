262b8bd6f473301dfdcbac5bb6aac11b115aff81
Rébecca <rebecca@codingtheweb.ca>
2025-02-12 19:11:43 -0500

    feat: basic functionality of weather api

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,8 +1,14 @@
+from typing import Optional
+
 import fastapi
+from fastapi import Depends
+
+from models.location import Location
+
 
 router = fastapi.APIRouter()
 
 
-@router.get("/api/weather")
-def weather():
-    return "Some report"
+@router.get("/api/weather/{city}")
+def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
+    return f"{loc.city}, {loc.state}, {loc.country} in {units}"
